cf0748550c1ed4b5efc31fb6df878a505425343d
sadingc <103971417+sadingc@users.noreply.github.com>
2022-06-22 23:54:21 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+#### tg-bot-youtube-downloader
+a telegram bot for download youtube videos.
+
+1.Open `index.php` and set your API_TOKEN
+ 
+2.Call setWebhook by opening this url : `https://api.telegram.org/botAPI_TOKEN/setWebhook?url=https://yourdomain.com/index.php` 
+> change API_TOKEN and yourdomain.com in the above link
+ 
+3.Send `vid:YOUTUBE_VIDEO_ID` to your bot
+
+Example : 
+
+`vid:nCmUwepQrbA`
+
+## Notice :
+
+files saved in `videos/` directory will be deleted after 24 hours. you can change the time in `checkForOldFiles` in `Youtube.php` class.
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+$message = file_get_contents('php://input');
+
+require 'libs/Telegram.php';
+require 'libs/Youtube.php';
+
+use Smoqadam\Telegram;
+use Smoqadam\Youtube;
+
+$api_token = '5361076448:AAF7VKCKyl7-K-QF6NWhJcifBijLveg1Oho';
+
+$tg = new Telegram($api_token);
+$y = new Youtube();
+
+
+/**
+ * download a video by video Id
+ */
+$tg->cmd('vid:<<:any>>', function ($video_id, $option) use ($tg, $y, $message) {
+
+
+    if (!strlen($video_id)) {
+
+        $tg->sendMessage("vid:<youtube video ID>", $tg->getChatId());
+        return;
+    }
+
+    // get video information and initial
+    if($y->init($video_id) !== false){
+
+    	$tg->sendMessage("Please wait ...", $tg->getChatId());
+    	//$tg->sendMessage($y->getName(), $tg->getChatId());
+    	$msg = "Download finished! \n".$y->download();
+    }else{
+  	$msg = $y->getError();
+    }
+
+    $tg->sendMessage($msg, $tg->getChatId());
+
+    $y->checkForOldFiles();
+
+});
+
+/**
+ * delete file by video id
+ */
+$tg->cmd('del:<<:any>>', function ($video_id) use ($tg, $y) {
+
+    $y->init($video_id);
+    $y->deleteFile($y->getPath() . $y->getTitle() . '.mp4');
+    $msg = $y->getTitle() . ' Deleted!!';
+    $tg->sendMessage($msg, $tg->getChatId());
+
+});
+
+
+$tg->process(json_decode($message, true));
+
+
